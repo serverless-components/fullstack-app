18b65ce460344e37187e728f0115cc160817d614
ac360 <austencollins@gmail.com>
2020-07-03 16:58:08 -0700

    auto-add secret token for onboarding ease

--- api/config/passport.js
+++ api/config/passport.js
@@ -1,7 +1,7 @@
 /**
  * Config: Passport.js
  */
-  
+
 const StrategyJWT = require('passport-jwt').Strategy
 const ExtractJWT = require('passport-jwt').ExtractJwt
 const { users } = require('../models')
@@ -11,14 +11,14 @@ module.exports = (passport) => {
 
   const options = {}
   options.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken()
-  options.secretOrKey = process.env.tokenSecret
+  options.secretOrKey = process.env.tokenSecret || 'secret_j91jasf0j1asfkl' // Change this to only use your own secret token
 
   passport.use(new StrategyJWT(options, async (jwtPayload, done) => {
     let user
     try { user = await users.getById(jwtPayload.id) }
-    catch (error) { 
+    catch (error) {
       console.log(error)
-      return done(error, null) 
+      return done(error, null)
     }
 
     if (!user) { return done(null, false) }
